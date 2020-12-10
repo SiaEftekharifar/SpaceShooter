f01f26476e002d11fb52cc6c97ee1cd84dd7b516
Siavash Eftekharifar <siavash.eftekharifar1991@gmail.com>
2020-12-09 21:02:13 -0500

    Added_SpaceShip_SkyBox_SplashLevel

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+
+    [SerializeField] int delayTime;
+
+    void Start()
+    {
+        Invoke("GotoMainLevel", delayTime);
+    }
+
+    private void GotoMainLevel() {
+
+        SceneManager.LoadScene(1);
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
